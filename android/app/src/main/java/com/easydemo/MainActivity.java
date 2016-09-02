57cd6d2a80872e091762b817c8ba3dac5e8ffe01
howie-misfit <howie@misfit.com>
2016-09-02 16:50:01 +0800

    Use Customized React Activity.
    - Remove React Application.

--- android/app/src/main/java/com/easydemo/MainActivity.java
+++ android/app/src/main/java/com/easydemo/MainActivity.java
@@ -1,16 +1,90 @@
 package com.easydemo;
 
-import com.facebook.react.ReactActivity;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
 
-public class MainActivity extends ReactActivity {
+import com.facebook.react.LifecycleState;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
+
+public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
+    private ReactRootView mReactRootView;
+    private ReactInstanceManager mReactInstanceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mReactRootView = new ReactRootView(this);
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModuleName("index.android")
+                .addPackage(new MainReactPackage())
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .setUseOldBridge(true)
+                .build();
+        mReactRootView.startReactApplication(mReactInstanceManager, "EasyDemo", null);
+
+        setContentView(mReactRootView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostResume(this, this);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostDestroy();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onBackPressed();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+            mReactInstanceManager.showDevOptionsDialog();
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
+     * By default, all onBackPress() calls should not execute the default backpress handler and should
+     * instead propagate it to the JS instance. If JS doesn't want to handle the back press itself,
+     * it shall call back into native to invoke this function which should execute the default handler
      */
     @Override
-    protected String getMainComponentName() {
-        return "EasyDemo";
+    public void invokeDefaultOnBackPressed() {
+
     }
 }
